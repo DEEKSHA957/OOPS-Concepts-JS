eeca396e2069733807d573ef9980861c94eab3c7
DEEKSHA957 <92008362+DEEKSHA957@users.noreply.github.com>
2021-11-02 16:10:17 +0530

    okie

---
+++ size.java
@@ -0,0 +1,12 @@
+class size
+{
+    public static void main(String[] args)
+    {
+        int arr[]={38,40,42};
+        char []size={'s','m','l'};
+        String[] enames={"ram","roy","sam"};
+        System.out.println(arr.length);
+        System.out.println(size.length);
+        System.out.println(enames.length);
+    }
+}

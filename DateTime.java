d3c70a31c69dacd807c12f09751f76551525aa37
DEEKSHA957 <92008362+DEEKSHA957@users.noreply.github.com>
2021-12-22 13:03:17 +0530

    commit

---
+++ DateTime.java
@@ -0,0 +1,10 @@
+import java.util.*;
+  
+public class DateTime {
+    public static void main(String args[])
+    {
+        Date current_Date = new Date();
+System.out.println(current_Date);
+        
+    }
+}

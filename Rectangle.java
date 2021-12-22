e7d0466128330411c29b10b79a5d806aa82b33dd
DEEKSHA957 <92008362+DEEKSHA957@users.noreply.github.com>
2021-12-22 23:38:14 +0530

    commit

---
+++ Rectangle.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+ class Rectangle {
+	public static void main(String[] args) { 
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the length of rectangle");
+		double length = sc.nextDouble();
+		System.out.println("Enter the width of rectangle");
+		double width = sc.nextDouble();		
+		double area = length * width;
+		System.out.println("Area of Rectangle is: " + area);
+		
+		sc.close();
+	}
+}

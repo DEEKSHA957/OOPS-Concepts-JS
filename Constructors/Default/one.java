c5838820d0981836df9a7390ccdbe6ef029fd39d
DEEKSHA957 <92008362+DEEKSHA957@users.noreply.github.com>
2022-01-06 23:16:41 +0530

    commit

---
+++ Constructors/Default/one.java
@@ -0,0 +1,10 @@
+ public class one
+ {
+ public static void main(String argvs[])
+ {
+     // creating two objects
+     one obj1 = new one();
+     one obj2 = new one();
+      }
+ } 
+ //no output but it is a perfect code

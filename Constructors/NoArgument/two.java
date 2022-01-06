0f67fc55e554473803a26347396c24d115aa61da
DEEKSHA957 <92008362+DEEKSHA957@users.noreply.github.com>
2022-01-06 23:13:09 +0530

    commit

---
+++ Constructors/NoArgument/two.java
@@ -0,0 +1,12 @@
+ public class two
+ {
+ // no argument constructor
+ two()
+ {
+ System.out.println("Inside the no argument constructor. ");
+ }
+ public static void main(String argvs[])
+ {
+     two obj1 = new two();
+ }
+ }
